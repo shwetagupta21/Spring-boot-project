9216cd2538c91cef08651dd7eedd304f2252218c
Shweta <Shweta@code>
2023-01-14 22:48:01 +0530

    EntityClass Added

---
+++ src/main/java/com/springproject/Springbootproject/entity/Department.java
@@ -0,0 +1,91 @@
+package com.springproject.Springbootproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Entity  // particular department class now can intreract with database.
+@Data
+//@Setter
+//@Getter
+@AllArgsConstructor //we can remove  parameterized constructor
+@NoArgsConstructor //we can remove Non parameterized constructor
+@Builder
+public class Department {
+    @Id       //to make department ID as a primary key.
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+
+    @NotBlank(message = "Please Add Department Name")
+  //  @Length(max=15,min=5)
+
+//    @Size(max=10,min=0)
+//    @Email
+//    @Positive
+//    @Negative
+//    @PositiveOrZero
+//    @NegativeOrZero
+//    @Future
+//    @FutureOrPresent
+//    @Past
+//    @PositiveOrZero
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+//    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+//        this.departmentId = departmentId;
+//        this.departmentName = departmentName;
+//        this.departmentAddress = departmentAddress;
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department() {
+//    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId=" + departmentId +
+                ", departmentName='" + departmentName + '\'' +
+                ", departmentAddress='" + departmentAddress + '\'' +
+                ", departmentCode='" + departmentCode + '\'' +
+                '}';
+    }
+}
